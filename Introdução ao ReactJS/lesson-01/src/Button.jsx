da905b0bc2d978b064e892394368b72a4790cf6e
TiagoTNX <tiagotnx@gmail.com>
2022-03-03 00:48:29 -0300

    Primeira aula

---
+++ Introdução ao ReactJS/lesson-01/src/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Button (props) {
+
+    const { name, onClick } = props
+
+    return (
+        <button onClick={onClick}>
+            {name}
+        </button>
+    )
+}
+
+export default Button;
--- Introdução ao ReactJS/lesson-01/src/index.js
+++ Introdução ao ReactJS/lesson-01/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Button from './Button'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Button name='Tiago' />
   </React.StrictMode>,
   document.getElementById('root')
 );
